bf25f823eb59fc7782e474118bec051fd3556b70
sunehachoudhary <sunehachoudhary47@gmail.com>
2026-05-30 18:10:08 +0530

    Revert "admin role added"

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -30,17 +30,4 @@ const protect = async(req, res, next) => {
   }
 };
 
-const admin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
-    next();
-  } else {
-    res.status(403).json({
-      message: "Admin access only",
-    });
-  }
-};
-
-module.exports = {
-  protect,
-  admin,
-};
+module.exports = protect;
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,11 +14,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-  
 });
 
 module.exports = mongoose.model("User", userSchema);
