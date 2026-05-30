726323a8aad0024c18fb50c29a8b73d28755f4bb
sunehachoudhary <sunehachoudhary47@gmail.com>
2026-05-30 18:04:40 +0530

    admin role added

--- backend/routes/queueRoutes.js
+++ backend/routes/queueRoutes.js
@@ -17,7 +17,7 @@ router.post("/", protect, createQueue);
 router.get("/", protect, getQueues);
 router.get("/:id", protect, getQueueById);
 router.put("/:id",protect,updateQueue);
-router.delete("/:id", protect, deleteQueue);
+router.delete("/:id", protect, admin, deleteQueue);
 router.get("/stats/:id", protect, getQueueStats);
 
 module.exports = router;
