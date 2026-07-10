147954444fe6191ae68a84e20fa8bc85f0b1b75c
Vanshika Varshney <vanshikavarshney741@gmail.com>
2026-07-10 15:19:25 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,6 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
-console.log("Loaded MONGO_URI:", process.env.MONGO_URI);
-
 const app = express();
 app.use(cors());
 app.use(express.json());
